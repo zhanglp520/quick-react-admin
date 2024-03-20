8c6e1f9c1805e857d822e728b011cfb1b85e7b24
jessica <3344538341@qq.com>
2024-03-20 09:32:23 +0800

    echarts完成

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -2,42 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Avatar, Card, Flex } from "antd";
 import "./index.less";
 
-// import Echarts1 from "./components/echarts1";
-import ReactEcharts from "echarts-for-react";
-import * as echarts from "echarts";
-type SizeType = ConfigProviderProps["componentSize"];
+import Echarts1 from "./components/echarts1";
+
 const { Meta } = Card;
 const baseStyle: React.CSSProperties = {
   width: "25%",
   height: 154,
 };
 const Home: React.FC = () => {
-  let myChart = null;
-
-  useEffect(() => {
-    myChart = echarts.init(document.getElementById("main"));
-    const option = {
-      // ECharts 配置项
-      title: {
-        text: "ECharts 示例",
-      },
-      tooltip: {},
-      xAxis: {
-        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
-      },
-      yAxis: {},
-      series: [
-        {
-          name: "销量",
-          type: "bar",
-          data: [5, 20, 36, 10, 10, 20],
-        },
-      ],
-    };
-
-    // 使用刚指定的配置项和数据显示图表。
-    myChart.setOption(option);
-  }, []);
   return (
     <div className="box">
       <div className="item-top">
@@ -53,7 +25,7 @@ const Home: React.FC = () => {
             <Meta
               avatar={<Avatar src="@/assets/images/login-bg.jpeg" />}
               title="4242"
-              description="当日订单数量"
+              description="当日订单总量"
             />
           </Card>
           <Card
@@ -67,7 +39,7 @@ const Home: React.FC = () => {
             <Meta
               avatar={<Avatar src="@/assets/images/login-bg.jpeg" />}
               title="9814"
-              description="昨日订单量"
+              description="昨日订单总量"
             />
           </Card>
           <Card
@@ -81,7 +53,7 @@ const Home: React.FC = () => {
             <Meta
               avatar={<Avatar src="@/assets/images/login-bg.jpeg" />}
               title="18522"
-              description="本周订单量"
+              description="本周订单总量"
             />
           </Card>
           <Card
@@ -95,13 +67,18 @@ const Home: React.FC = () => {
             <Meta
               avatar={<Avatar src="@/assets/images/login-bg.jpeg" />}
               title="99999"
-              description="本月订单数量"
+              description="本月订单数总量"
             />
           </Card>
         </Flex>
       </div>
-      <div id="main" style={{ width: 500, height: 400 }}></div>
-      {/* <Echarts1></Echarts1> */}
+      <div className="content-wrap">
+        <div className="content-echart">
+          <Echarts1></Echarts1>
+        </div>
+        <div className="content-deal">成交订单详情</div>
+        <div className="content-Loss">流失订单详情</div>
+      </div>
     </div>
   );
 };
