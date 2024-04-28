623025deb1b7834cd070cbb661f67824e0d0ab0b
jessica <3344538341@qq.com>
2024-04-28 11:42:10 +0800

    refactor：重构路由

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,15 +1,91 @@
-// import { createBrowserRouter } from "react-router-dom";
-// import staticRouter from "./staticRouter";
-// import { addRoutes } from "./dynamicRouter";
+import { useRoutes, RouteObject, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store";
+import { IMenubar } from "@/types";
+import { lazy, Suspense } from "react";
+import Layout from "@/layout/index";
+import Home from "@/views/home";
+import Login from "@/pages/login";
+import ChangePassword from "@/views/changePassword";
+import NotFound from "@/pages/404";
 
-// const routerData = addRoutes();
-// const routes = [...staticRouter];
-// routes[0].children?.push(...routerData);
-// export const router = createBrowserRouter([...routes]);
-// console.log("router.routes", router.routes);
+const Router = () => {
+  const user = useSelector((state: RootState) => state.user);
+  let menuList: IMenubar[] = [];
+  if (user) {
+    menuList = user.menuList;
+  }
+  const routes: RouteObject[] = [
+    {
+      path: "/",
+      element: <Navigate to="/home" />,
+    },
+    {
+      path: "/home",
+      element: <Layout />,
+      children: [
+        {
+          path: "/home",
+          element: <Home />,
+        },
+      ],
+    },
 
-import { createBrowserRouter } from "react-router-dom";
-import staticRouter from "./staticRouter";
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/changePassword",
+      element: <Layout />,
+      children: [
+        {
+          path: "/changePassword/index",
+          element: <ChangePassword />,
+        },
+      ],
+    },
+    // {
+    //   path: "/:catchAll(.*)",
+    //   element: <NotFound />,
+    // },
+  ];
 
-export const router = createBrowserRouter([...staticRouter]);
-console.log('router',router);
+  const genRouter = (menuList: IMenubar[], routes: RouteObject[]) => {
+    menuList.map((ele: IMenubar) => {
+      const arr: RouteObject[] = [];
+      let path = "";
+      path = ele.path;
+      if (ele.children && ele.children.length > 0) {
+        ele.children.map((item: IMenubar) => {
+          arr.push({
+            path: item.path,
+            element: LazyWrapper(item.path),
+          });
+        });
+      }
+      routes.push({
+        path: path,
+        element: <Layout />,
+        children: arr,
+      });
+    });
+  };
+  const LazyWrapper = (path: any) => {
+    const Component = lazy(() => import(`../views${path}`));
+    return (
+      <Suspense>
+        <Component />
+      </Suspense>
+    );
+  };
+  genRouter(menuList, routes);
+  routes.push({
+    path: "/:catchAll(.*)",
+    element: <NotFound />,
+  });
+  const element = useRoutes(routes);
+  console.log("路由", routes);
+  return <>{element}</>;
+};
+export default Router;
