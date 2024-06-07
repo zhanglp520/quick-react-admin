66d29a8b6d527f1a1355ec98029447d27dd1c3c2
土豆哥 <zhanglp15229380174@163.com>
2024-06-07 18:34:24 +0800

    pref: 接口地址申请域名

--- vite.config.ts
+++ vite.config.ts
@@ -25,9 +25,8 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/dev-api\/developer-tools/, ""), // 路径重写
       },
       "/dev-api": {
-        target: "http://192.168.2.5:3101/", // 代理的目标地址-本地
-        // target: "https://api.quick.ainiteam.com/", // 代理的目标地址-线上域名
-        // target: "http://82.156.137.92:3101/", // 代理的目标地址-线上
+        // target: "http://192.168.2.5:3101/", // 代理的目标地址-本地开发
+        target: "https://react.api.quick.ainiteam.com/", // 代理的目标地址-线上域名
         changeOrigin: true, // 开发模式，默认的origin是真实的 origin:localhost:3102 代理服务会把origin修改为目标地址
         secure: true, // 是否https接
         ws: false, // 是否代理websockets
